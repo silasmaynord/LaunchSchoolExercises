ff6ff8c7210316dcd97251476555a2c8d51ad80c
Silas Maynord <scmaynord23@gmail.com>
2017-02-14 22:20:13 -0600

    added more Learn to Program Exercises

--- LearntoProgram/1.7.rb
+++ LearntoProgram/1.7.rb
@@ -104,4 +104,132 @@ end
 
 words = ['can', 'feel','singing.','like','A','can']
 puts(dictionary_sort(words).join(' '))
-  
+
+#Expanded english_number 
+def english_number number 
+  if number < 0 
+    return "Please enter a non-negative number"
+  end 
+  if number == 0 
+    return 'zero'
+  end 
+  num_string = ''
+  ones_place = ['one', 'two', 'three',
+                'four','five','six',
+                'seven','eight','nine']
+  tens_place = ['ten','twenty','thirty',
+                'fourty','fifty','sixty',
+                'seventy','eighty','ninety']
+  teenagers  = ['eleven','twelve','thirteen'
+                'fourteen','fifteen','sixteen',
+                'seventeen','eighteen','nineteen']
+  zillions   = [['hundred',            2]
+                ['thousand',           3]
+                ['million',            6]
+                ['billion',            9]
+                ['trillion',          12]
+                ['quadrillion',       15]
+                ['quintillion',       18]
+                ['sextillion',        21]
+                ['septillion',        24]
+                ['octillion',         27]
+                ['nonillion',         30]
+                ['decillion',         33]
+                ['undecillion',       36]
+                ['duodecillion',      39]
+                ['tredecillion',      42]
+                ['quattourdecillion', 45]
+                ['quindecillion',     48]
+                ['sexdecillion',      51]
+                ['septendecillion',   54]
+                ['octdecillion',      57]
+                ['novemdecillion',    60]
+                ['vigintillion',      63]
+                ['googol',           100]]
+  left = number
+
+  while zillions.length > 0 
+    zil_pair =        zillions.pop
+    zil_name =        zil_pair[0]
+    zil_base =    10**zil_pair[1]
+    write = left/zil_base           #How many zillions left?
+    left = left - write*zil_base    #Subract off those zillions 
+
+    if write > 0 
+      #Here's the recursion
+      prefix = english_number write
+      num_string = num_string + prefix + ' ' + zil_name
+
+      if left > 0 
+        #So we don't write 'two billionfifty-one' 
+        num_string = num_string + ' '
+      end 
+    end 
+  end 
+  write = left/10                   #How many tens left?
+  left = left - write*10            #Subtract off those tens
+
+  if write > 0
+    if ((write == 1) and (left > 0))
+      #Since we can't write 'tenty-two' instead of twelve
+      #We have to make a special exception for these 
+      num_string = num_string + teenagers[left - 1]
+      #the '-1' is to adject for index position
+
+      #Digit in the ones place is already taken care of 
+      left = 0 
+    else 
+      num_string = num_string + tens_place[write - 1]
+      #To adjust for index position
+    end 
+
+    if left > 0 
+      #So we don't write 'sixtyfour'
+      num_string = num_string + ' '
+    end 
+  end 
+
+  write = left                    #How many ones left? 
+  left = 0                        #Subract off those ones 
+
+  if write > 0 
+    num_string = num_string + ones_place[write-1]
+    #to adjust for index position
+  end 
+  num_string 
+end 
+
+puts english_number(0)
+
+#99 Bottles of Beer on the Wall 
+
+#english_number plus this: 
+num_at_start = 99
+num_now = num_at_start
+while num_now > 2 
+  puts english_number(num_now).capitalize + 'bottles of beer on the wall'
+    english_number(num_now) + ' bottles of beer'
+  num_now = num_now - 1 
+  puts "take one down, pass it around, " + 
+    english_number(num_now) + " bottles of beer on the wall"
+end 
+
+puts "Two bottles of beer on the wall, two bottles of beer"
+puts "take one down, pass it around, one bottle of beer on the wall"
+puts "one bottle of beer on the wall, one bottle of beer"
+puts "take it down, and pass it around, no more bottles of beer on the wall"
+
+#or 
+num_at_start = 99
+
+num_bot = proc {|n| "#{english_number n} bottle#{n == 1 ? '' : 's'}"}
+
+num_at_start.downto(2) do |num| 
+  bottles = 
+  puts "#{num_bot[num]} of beer on the wall, #{num_bot[num]} of beer".capitalize
+  puts "take one down, pass it around, #{num_bot[num]-1} of beer on the wall"
+end 
+puts "#{num_bot[1]} of beer on the wall, #{num_bot[1]} of beer".capitalize
+puts "take one down, pass it around, no more bottles of beer on the wall"
+
+
